7274937df3065f46efee20344076dc3c73940f6a
WenDEVLIFE <frouenmedinajr@gmail.com>
2026-05-14 12:23:35 +0800

    feat: add "use client" directive to TeacherProfileView component

--- features/teacher-profile/TeacherProfileView.tsx
+++ features/teacher-profile/TeacherProfileView.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useState } from "react";
 import { TeacherPersonalInfo } from "./components/TeacherPersonalInfo";
 import { TeacherAcademicStats } from "./components/TeacherAcademicStats";
