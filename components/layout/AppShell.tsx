0326fef9fe6db95e3666615a62f00cebf952d028
WenDEVLIFE <frouenmedinajr@gmail.com>
2026-05-20 07:31:51 +0800

    added mobile responsive

--- components/layout/AppShell.tsx
+++ components/layout/AppShell.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import * as React from "react";
 import { Sidebar } from "./Sidebar";
 import { Header } from "./Header";
@@ -8,12 +10,26 @@ interface AppShellProps {
 }
 
 export function AppShell({ children }: AppShellProps) {
+  const [sidebarOpen, setSidebarOpen] = React.useState(false);
+
   return (
     <div className="flex h-screen bg-bg overflow-hidden font-manrope relative">
-      <Sidebar />
+      {/* Mobile overlay */}
+      {sidebarOpen && (
+        <div 
+          className="fixed inset-0 z-40 bg-black/50 md:hidden"
+          onClick={() => setSidebarOpen(false)}
+          aria-hidden="true"
+        />
+      )}
+
+      {/* Sidebar */}
+      <Sidebar open={sidebarOpen} onClose={() => setSidebarOpen(false)} />
+
+      {/* Main content */}
       <div className="flex flex-1 flex-col overflow-hidden">
-        <Header />
-        <main className="flex-1 overflow-y-auto p-8">
+        <Header onMenuClick={() => setSidebarOpen(!sidebarOpen)} />
+        <main className="flex-1 overflow-y-auto p-4 sm:p-6 md:p-8">
           <div className="mx-auto max-w-7xl">
             {children}
           </div>
